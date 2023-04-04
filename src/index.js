14a5e0600fe58b1aa0a25cde466f19ede9b7085c
Melita <mebosnjak@gmail.com>
2023-04-04 21:08:10 +0200

    creates a root running in React 18, which adds all of the improvements of React 18 and allows you to use concurrent features.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
 
-// work offline and load faster with register SW
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
+
 serviceWorker.register();
